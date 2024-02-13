9a041d8880a9e0d787ef2b29d5cfe62096eadb90
Brendan Molloy <brendan@bbqsrc.net>
2024-02-13 14:45:59 +0100

    Make build on Windows

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
+#[cfg(unix)]
 fn main() {
     println!("cargo:rustc-link-lib=hfst");
+
+    // let include = std::env::var("HFST_INCLUDE").ok();
 
     cc::Build::new()
         .file("wrapper/wrapper.cpp")
@@ -8,5 +11,21 @@ fn main() {
         .static_flag(true)
         .cpp(true)
         .flag("-std=c++11")
+        .compile("hfst_wrapper");
+}
+
+#[cfg(windows)]
+fn main() {
+    use std::path::PathBuf;
+
+    println!("cargo:rustc-link-lib=hfst");
+
+    let sysroot = PathBuf::from(std::env::var("SYSROOT").unwrap());
+
+    cc::Build::new()
+        .file("wrapper/wrapper.cpp")
+        .include(sysroot.join("hfst"))
+        .include(&sysroot)
+        .cpp(true)
         .compile("hfst_wrapper");
 }
