af636a2aa5c1cbbed92b42bc11181d5dd35e1cef
Brendan Molloy <brendan@necessary.nu>
2024-02-21 23:07:48 +0100

    Enough violence was enough

--- build.rs
+++ build.rs
@@ -1,41 +1,17 @@
-fn main() {
-    // println!("cargo:rustc-link-lib=hfst");
+use std::path::Path;
 
+fn main() {
     let dst = cmake::Config::new("lib").always_configure(true).no_build_target(true).build();
     println!("cargo:rustc-link-search=native={}/libhfst", dst.display());
 
     cc::Build::new()
         .file("wrapper/wrapper.cpp")
         // .include("/usr/local/include/hfst")
-        // .include("/opt/homebrew/include")
-        .include(dst.join("include"))
+        .include("/opt/homebrew/include")
+        .include(Path::new("lib/libhfst/src"))
         .static_flag(true)
         .cpp(true)
         .flag("-std=c++11")
         .compile("hfst_wrapper");
-// }
-// fn main() {
-    // let icu = pkg_config::Config::new()
-    //     .statik(true)
-    //     .probe("icu-uc")
-    //     .unwrap();
-    // for path in icu.link_paths {
-    //     println!("cargo:rustc-link-search=native={}", path.display());
-    // }
 
-    // println!("cargo:rustc-link-lib=static=cg3");
-    // println!("cargo:rustc-link-lib=static=icuuc");
-    // println!("cargo:rustc-link-lib=static=icuio");
-    // println!("cargo:rustc-link-lib=static=icudata");
-    // println!("cargo:rustc-link-lib=static=icui18n");
-
-    // cc::Build::new()
-    //     .file("wrapper/wrapper.cpp")
-    //     .include(dst.join("include"))
-    //     .include(dst.join("include").join("cg3"))
-    //     .include(dst)
-    //     .static_flag(true)
-    //     .cpp(true)
-    //     .flag("-std=c++11")
-    //     .compile("cg3_wrapper");
 }
