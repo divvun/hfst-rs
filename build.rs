004ca9573e265f1bec439408e6cb9add834f86dc
Brendan Molloy <brendan@necessary.nu>
2024-02-21 22:29:41 +0100

    bah

--- build.rs
+++ build.rs
@@ -1,33 +1,41 @@
-#[cfg(unix)]
 fn main() {
-    println!("cargo:rustc-link-lib=hfst");
+    // println!("cargo:rustc-link-lib=hfst");
 
-    // let include = std::env::var("HFST_INCLUDE").ok();
+    let dst = cmake::Config::new("lib").always_configure(true).no_build_target(true).build();
+    println!("cargo:rustc-link-search=native={}/libhfst", dst.display());
 
     cc::Build::new()
         .file("wrapper/wrapper.cpp")
-        .include("/usr/local/include/hfst")
-        .include("/opt/homebrew/include")
+        // .include("/usr/local/include/hfst")
+        // .include("/opt/homebrew/include")
+        .include(dst.join("include"))
         .static_flag(true)
         .cpp(true)
         .flag("-std=c++11")
         .compile("hfst_wrapper");
-}
+// }
+// fn main() {
+    // let icu = pkg_config::Config::new()
+    //     .statik(true)
+    //     .probe("icu-uc")
+    //     .unwrap();
+    // for path in icu.link_paths {
+    //     println!("cargo:rustc-link-search=native={}", path.display());
+    // }
 
-#[cfg(windows)]
-fn main() {
-    use std::path::PathBuf;
+    // println!("cargo:rustc-link-lib=static=cg3");
+    // println!("cargo:rustc-link-lib=static=icuuc");
+    // println!("cargo:rustc-link-lib=static=icuio");
+    // println!("cargo:rustc-link-lib=static=icudata");
+    // println!("cargo:rustc-link-lib=static=icui18n");
 
-    let sysroot = PathBuf::from(std::env::var("SYSROOT").unwrap());
-
-    println!("cargo:rustc-link-search=native={}/lib", sysroot.display());
-    println!("cargo:rustc-link-search=native={}/bin", sysroot.display());
-    println!("cargo:rustc-link-lib=dylib=hfst");
-
-    cc::Build::new()
-        .file("wrapper/wrapper.cpp")
-        .include(sysroot.join("include").join("hfst"))
-        .include(sysroot.join("include"))
-        .cpp(true)
-        .compile("hfst_wrapper");
+    // cc::Build::new()
+    //     .file("wrapper/wrapper.cpp")
+    //     .include(dst.join("include"))
+    //     .include(dst.join("include").join("cg3"))
+    //     .include(dst)
+    //     .static_flag(true)
+    //     .cpp(true)
+    //     .flag("-std=c++11")
+    //     .compile("cg3_wrapper");
 }
