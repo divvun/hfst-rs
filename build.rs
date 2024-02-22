32d4363b9098639f166c9585b3934a42de1991e8
Brendan Molloy <brendan@necessary.nu>
2024-02-22 13:47:49 +0100

    Add vcpkg

--- build.rs
+++ build.rs
@@ -1,18 +1,34 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let dst = cmake::Config::new("lib").always_configure(true).no_build_target(true).build();
-    println!("cargo:rustc-link-search=native={}/build/libhfst", dst.display());
+    let includes = if cfg!(windows) {
+        let lib = vcpkg::Config::new()
+            .find_package("icu4c")
+            .unwrap();
+        lib.include_paths
+    } else if cfg!(target_os = "macos") {
+        vec![PathBuf::from("/opt/homebrew/include")]
+    } else {
+        vec![]
+    };
+
+    let dst = cmake::Config::new("lib")
+        .always_configure(true)
+        .define("CMAKE_CXX_FLAGS", "/EHsc /O2")
+        .no_build_target(true)
+        .build();
+    println!(
+        "cargo:rustc-link-search=native={}/build/libhfst",
+        dst.display()
+    );
     println!("cargo:rustc-link-lib=static=hfst");
 
     cc::Build::new()
         .file("wrapper/wrapper.cpp")
-        // .include("/usr/local/include/hfst")
-        // .include("/opt/homebrew/include")
+        .includes(includes)
         .include(Path::new("lib/libhfst/src"))
         .static_flag(true)
         .cpp(true)
         // .flag("-std=c++11")
         .compile("hfst_wrapper");
-
 }
