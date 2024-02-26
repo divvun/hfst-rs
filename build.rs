9f76661e8ea8ba29bfad2540cf41b38b4089f7b2
Brendan Molloy <brendan@necessary.nu>
2024-02-26 21:02:36 +0100

    Make Linux happy, including aarch64

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let (includes, libs) = if cfg!(windows) {
@@ -28,21 +28,49 @@ fn main() {
                 .display()
         );
         println!("cargo:rustc-link-lib=hfst");
-        // println!("cargo:rustc-link-lib=icuuc");
-        // println!("cargo:rustc-link-lib=icuio");
-        // println!("cargo:rustc-link-lib=icudt");
-        // println!("cargo:rustc-link-lib=icuin");
-    } else if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-lib=icuuc");
+        println!("cargo:rustc-link-lib=icuio");
+        println!("cargo:rustc-link-lib=icudt");
+        println!("cargo:rustc-link-lib=icuin");
+    }
+
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
+    }
+
+    if cfg!(unix) {
         println!(
             "cargo:rustc-link-search=native={}",
             dst.join("build").join("libhfst").display()
         );
-        println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
         println!("cargo:rustc-link-lib=static=hfst");
         println!("cargo:rustc-link-lib=static=icuuc");
         println!("cargo:rustc-link-lib=static=icuio");
         println!("cargo:rustc-link-lib=static=icudata");
         println!("cargo:rustc-link-lib=static=icui18n");
+    }
+
+    if cfg!(target_os = "linux") {
+        let o = std::process::Command::new("ld")
+            .arg("--verbose")
+            .output()
+            .unwrap();
+        let out = std::str::from_utf8(&o.stdout).unwrap();
+        for line in out.lines() {
+            if line.starts_with("SEARCH_DIR") {
+                let iter = line.split(";").map(|x| {
+                    x.trim()
+                        .trim_start_matches("SEARCH_DIR(\"=")
+                        .trim_end_matches("\")")
+                });
+                for i in iter {
+                    if Path::new(i).exists() {
+                        println!("cargo:rustc-link-search=native={}", i);
+                    }
+                }
+                break;
+            }
+        }
     }
 
     let is_shared = cfg!(windows) && std::env::var("VCPKGRS_DYNAMIC").is_ok();
